4a9b41ddc86bdc20ad99a7ca40468e84f2af4072
Andreas Ritter <andreasholteritter@gmail.com>
2020-02-25 15:39:11 +0100

    Initialized controller

--- src/main/java/no/ritter/tree/controller/TreeController.java
+++ src/main/java/no/ritter/tree/controller/TreeController.java
@@ -1,4 +1,20 @@
 package no.ritter.tree.controller;
 
+import no.ritter.tree.service.TreeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class TreeController {
+
+    @Autowired
+    TreeService service;
+
+    @RequestMapping("/")
+    public @ResponseBody
+    String greeting() {
+        return "Hello, World";
+    }
 }
