a959ea352bf7afa4fe318e91c83aac928f871525
Andreas Ritter <andreasholteritter@gmail.com>
2020-02-25 15:36:30 +0100

    Added service bean reference

--- src/main/java/no/ritter/tree/TreeApplication.java
+++ src/main/java/no/ritter/tree/TreeApplication.java
@@ -1,10 +1,13 @@
 package no.ritter.tree;
 
+import no.ritter.tree.service.TreeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TreeApplication {
+
+	private TreeService treeService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TreeApplication.class, args);
