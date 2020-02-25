6c28f636e56ae109581fc485da1b303e8b3fa388
Andreas Ritter <andreasholteritter@gmail.com>
2020-02-25 15:33:00 +0100

    Added config

--- src/main/java/no/ritter/tree/AppConfig.java
+++ src/main/java/no/ritter/tree/AppConfig.java
@@ -1,4 +1,17 @@
 package no.ritter.tree;
 
+import no.ritter.tree.repository.TreeRepository;
+import no.ritter.tree.service.TreeService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
 public class AppConfig {
+
+    @Bean
+    @Profile("main")
+    public TreeRepository treeService() {
+        return new TreeService();
+    }
 }
--- src/main/java/no/ritter/tree/MyApplicationContextInitializer.java
+++ src/main/java/no/ritter/tree/MyApplicationContextInitializer.java
@@ -1,4 +1,15 @@
 package no.ritter.tree;
 
-public class MyApplicationContextInitializer {
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+public class MyApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext ac) {
+        String profile = "main";
+        ConfigurableEnvironment ce = ac.getEnvironment();
+        ce.addActiveProfile(profile);
+    }
 }
