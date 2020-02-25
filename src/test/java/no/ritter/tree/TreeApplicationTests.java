ab270ba226df5fc28dc1feea055ef5389b4a41ab
Andreas Ritter <andreasholteritter@gmail.com>
2020-02-25 14:52:58 +0100

    Initial commit, added project structure

---
+++ src/test/java/no/ritter/tree/TreeApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ritter.tree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
