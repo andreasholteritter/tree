51dd5859f49d3970edcee723f081032ef2fd3378
Andreas Ritter <andreasholteritter@gmail.com>
2020-03-05 01:33:35 +0100

    Added create and get test

--- src/test/java/no/ritter/tree/TreeApplicationTests.java
+++ src/test/java/no/ritter/tree/TreeApplicationTests.java
@@ -1,5 +1,7 @@
 package no.ritter.tree;
 
+import com.google.gson.Gson;
+import no.ritter.tree.entity.Tree;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -11,6 +13,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -37,4 +43,31 @@ class TreeApplicationTests {
 				.andExpect(content().string(equalTo("Hello world")));
 	}
 
+	@Test
+	public void createAndGet() throws Exception {
+		Tree tree = new Tree();
+		tree.setName("test tree");
+
+		Gson gson = new Gson();
+		String json = gson.toJson(tree);
+
+		MvcResult entityIdResult = mvc.perform(MockMvcRequestBuilders
+					.post("/tree/")
+					.accept(MediaType.APPLICATION_JSON)
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(json))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		int entityId = Integer.valueOf(entityIdResult.getResponse().getContentAsString());
+
+		MvcResult entityTreeResult = mvc.perform(MockMvcRequestBuilders.get("/tree/id/" + entityId).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String entityTree = entityTreeResult.getResponse().getContentAsString();
+
+		Tree returnTree = gson.fromJson(entityTree, Tree.class);
+		assertEquals(returnTree.getId(), entityId, "check if return id and id on return object matches");
+	}
 }
